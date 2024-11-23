534733440204d9e35dc1b5773de8354b02a7161e
Backbash <149115588+Backbash@users.noreply.github.com>
2024-11-23 08:21:46 -0500

    Revert "Merge branch 'main' of https://github.com/Backbash/NuttyB-Raptors"
    This reverts commit 4d52b938601beaf248a72c8474c27a36033f0be7, reversing
    changes made to 3ec3e7b0ae74e5ce83431f4e95e6706fbcb45488.

--- lua/tweakdefs1.hp2.lua
+++ lua/tweakdefs1.hp2.lua
@@ -1,42 +1,42 @@
 local a = {
-	health = 1505000,
-	repairable = false,
-	canbehealed = false,
-	buildtime = 9999999,
-	autoheal = 2,
-	canSelfRepair = false
+    health = 1505000,
+    repairable = false,
+    canbehealed = false,
+    buildtime = 9999999,
+    autoheal = 2,
+    canSelfRepair = false
 }
 for b, c in pairs(UnitDefs) do
-	if b == 'raptor_queen_epic' then
-		for d, e in pairs(a) do
-			c[d] = e
-		end
-	end
+    if b == "raptor_queen_epic" then
+        for d, e in pairs(a) do
+            c[d] = e
+        end
+    end
 end
 for f, g in pairs(UnitDefs) do
-	if string.sub(f, 1, 24) == 'raptor_land_swarmer_heal' then
-		g.reclaimspeed = 100
-		g.stealth = 0
-		g.builder = false
-		g.workertime = g.workertime * 0.65
-		g.canassist = 0
-	end
+    if string.sub(f, 1, 24) == "raptor_land_swarmer_heal" then
+        g.reclaimspeed = 100
+        g.stealth = 0
+        g.builder = false
+        g.workertime = g.workertime * 0.65
+        g.canassist = 0
+    end
 end
 for b, c in pairs(UnitDefs) do
-	if c.customparams and c.customparams.subfolder and c.customparams.subfolder == 'other/raptors' then
-		if c.health then
-			c.health = 2 * c.health
-		end
-	end
+    if c.customparams and c.customparams.subfolder and c.customparams.subfolder == "other/raptors" then
+        if c.health then
+            c.health = 2 * c.health
+        end
+    end
 end
 local h = UnitDef_Post
 function UnitDef_Post(i, j)
-	h(i, j)
-	for i, k in pairs(UnitDefs) do
-		if k.customparams and k.customparams.subfolder and k.customparams.subfolder == 'other/raptors' then
-			if k then
-				k.metalcost = k.health * .35
-			end
-		end
-	end
+    h(i, j)
+    for i, k in pairs(UnitDefs) do
+        if k.customparams and k.customparams.subfolder and k.customparams.subfolder == "other/raptors" then
+            if k then
+                k.metalcost = k.health * .35
+            end
+        end
+    end
 end
