1c58ea7b7c2e10e28f7e515d5c8549e68ba94c85
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-04-08 22:28:05 +0200

    late skateboard builder balance

--- lua/tweakdefs8.lua
+++ lua/tweakdefs8.lua
@@ -1,4 +1,4 @@
---WIP T3 builder v1
+--WIP T3 builder v2
 -- docs.google.com/spreadsheets/d/1QSVsuAAMhBrhiZdTihVfSCwPzbbZWDLCtXWP23CU0ko
 local unitDefs, unitDefName, factions, apt3 = UnitDefs or {}, 't3con', {'arm', 'cor', 'leg'}, 'apt3'
 for _, faction in pairs(factions) do
@@ -11,20 +11,21 @@ for _, faction in pairs(factions) do
 			buildpic = 'ARMDRONECARRY.DDS',
 			buildtime = 140000,
 			cruisealtitude = 130,
-			energycost = 200000,
+			energycost = 320000,
 			energyupkeep = 2000,
-			health = 10000,
+			health = 3000,
 			hoverattack = true,
 			idleautoheal = 5,
 			idletime = 1800,
-			maxthisunit = 3,
-			metalcost = 12600,
+			maxthisunit = 2,
+			metalcost = 20000,
 			objectname = 'Units/ARMDRONECARRY.s3o',
+			script = 'Units/ARMDRONECARRY.cob',
 			speed = 50,
 			terraformspeed = 3000,
 			turninplaceanglelimit = 360,
 			turnrate = 240,
-			workertime = 9000,
+			workertime = 15000,
 			customparams = {
 				subfolder = 'ArmAircraft/T3',
 				techlevel = 3,
