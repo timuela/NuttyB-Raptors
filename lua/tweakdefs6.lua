03319107391bc5ee19caf1740c967b5d7445ec79
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-04-15 23:53:38 +0200

    fix taxed building titles/descriptions

--- lua/tweakdefs6.lua
+++ lua/tweakdefs6.lua
@@ -1,16 +1,36 @@
 --Cross Faction Tax 70%
 -- Authors: TetrisCo
 -- docs.google.com/spreadsheets/d/1QSVsuAAMhBrhiZdTihVfSCwPzbbZWDLCtXWP23CU0ko
-local unitDefs, tax, t2Factories, taxedT2Factories = UnitDefs or {}, 1.7, {}, {}
+local unitDefs, tax, t2Factories, taxedT2Factories, unitI18N, _taxed, taxedTitlePostfix =
+	UnitDefs or {},
+	1.7,
+	{},
+	{},
+	Json.decode(VFS.LoadFile('language/en/units.json')),
+	'_taxed',
+	' (Taxed)'
+
 for name, def in pairs(unitDefs) do
 	if
 		def.customparams and def.customparams.subfolder and
 			(def.customparams.subfolder:match 'Fact' or def.customparams.subfolder:match 'Lab') and
 			def.customparams.techlevel == 2
 	 then
+		local unitTitle = unitI18N.units.names[name]
 		t2Factories[name] = 1
-		taxedT2Factories[name .. '_taxed'] =
-			table.merge(def, {metalcost = def.metalcost * tax, energycost = def.energycost * tax})
+		taxedT2Factories[name .. _taxed] =
+			table.merge(
+			def,
+			{
+				metalcost = def.metalcost * tax,
+				energycost = def.energycost * tax,
+				name = unitTitle .. taxedTitlePostfix,
+				customparams = {
+					i18n_en_humanname = unitTitle .. taxedTitlePostfix,
+					i18n_en_tooltip = unitI18N.units.descriptions[name]
+				}
+			}
+		)
 	end
 end
 
@@ -19,7 +39,7 @@ for builderName, T1T2BuilderDef in pairs(unitDefs) do
 		for _, buildoption in pairs(T1T2BuilderDef.buildoptions) do
 			if t2Factories[buildoption] then -- has non-cross-faction T2 factory
 				for _, faction in pairs {'arm', 'cor', 'leg'} do
-					local crossfactionTaxedOption = faction .. buildoption:sub(4) .. '_taxed'
+					local crossfactionTaxedOption = faction .. buildoption:sub(4) .. _taxed
 					if buildoption:sub(1, 3) ~= faction and taxedT2Factories[crossfactionTaxedOption] then
 						unitDefs[builderName].buildoptions[#unitDefs[builderName].buildoptions + 1] = crossfactionTaxedOption
 					end
