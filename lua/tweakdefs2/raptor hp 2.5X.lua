8bdaac247fb6325ab08aa9768b8ac86ec2f39d17
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-05-30 19:42:40 +0200

    add 2.5x

---
+++ lua/tweakdefs2/raptor hp 2.5X.lua
@@ -0,0 +1,30 @@
+--NuttyB v1.52 2.5X HP
+-- docs.google.com/spreadsheets/d/1QSVsuAAMhBrhiZdTihVfSCwPzbbZWDLCtXWP23CU0ko
+for f, g in pairs(UnitDefs) do
+	if string.sub(f, 1, 24) == 'raptor_land_swarmer_heal' then
+		g.reclaimspeed = 100
+		g.stealth = 0
+		g.builder = false
+		g.workertime = g.workertime * 0.6
+		g.canassist = 0
+		g.maxthisunit = 0
+	end
+	if
+		g.customparams and g.customparams.subfolder and g.customparams.subfolder == 'other/raptors' and g.health and
+			not f:match('^raptor_queen_.*')
+	 then
+		g.health = 2.5 * g.health
+	end
+end
+local h = UnitDef_Post
+function UnitDef_Post(i, j)
+	h(i, j)
+	for i, k in pairs(UnitDefs) do
+		if k.customparams and k.customparams.subfolder and k.customparams.subfolder == 'other/raptors' then
+			if k then
+				k.metalcost = math.floor(k.health * .34)
+				k.nochasecategory = "OBJECT"
+			end
+		end
+	end
+end
