fcc200eb85d50ce9382764b798f0c58bee1ae63b
rcorex <rodney.chua+rcore@gmail.com>
2025-07-04 14:00:55 +0700

    add post-queen doombringer wave and reduce egg drops
    - add post-queen Doombringer wave
    - add scaling with NuttyB QHP & HP multipliers
    - balance changes to wave sizes
    - reduced egg drops

--- lua/tweakdefs4.lua
+++ lua/tweakdefs4.lua
@@ -1,9 +1,27 @@
---Mini-Bosses
+--Mini-Bosses v2
 -- Authors: RCore
 -- docs.google.com/spreadsheets/d/1QSVsuAAMhBrhiZdTihVfSCwPzbbZWDLCtXWP23CU0ko
 local unitDefs, tableMerge, tableCopy, raptor_matriarch_basic, customfusionexplo, spring = UnitDefs or {}, table.merge, table.copy, 'raptor_matriarch_basic', 'customfusionexplo', Spring
 
--- Player scaling setup
+local nbQhpMult, nbHpMult = 1.0, 1.0
+do
+	local modOptions = spring.GetModOptions() or {}
+
+	local function findValue(tweakdefName, pattern)
+		local b64 = modOptions[tweakdefName]
+		if b64 and b64 ~= "" then
+			local ok, source = pcall(string.base64Decode, b64)
+			if ok and source then
+				return tonumber(string.match(source, pattern)) or 1.0
+			end
+		end
+		return 1.0
+	end
+
+	nbQhpMult = findValue('tweakdefs1', "(%d+%.?%d*)X QHP")
+	nbHpMult  = findValue('tweakdefs2', "(%d+%.?%d*)X HP")
+end
+
 local playerCountScale = 1
 if spring.Utilities.Gametype.IsRaptors() then
 	playerCountScale = (#spring.GetTeamList() - 2)/12
@@ -13,17 +31,22 @@ local function scaledMax(base)
 	return math.max(1, math.ceil(base * playerCountScale))
 end
 
--- Queen time scaling (baseline = 1.3) define and scale mqAnger
 local mqAnger = {70, 85, 90, 105, 110, 125}
 local mqTimeMult = math.max(1, spring.GetModOptions().raptor_queentimemult or 1.3)
 local mqStart, mqLast = mqAnger[1], mqAnger[#mqAnger]
-local mqTargetLast = mqTimeMult * 97
+local mqTargetLast = mqTimeMult * mqAnger[#mqAnger]/1.3
 local mqFactor = (mqTargetLast - mqStart) / (mqLast - mqStart)
 
 for i = 2, #mqAnger do
     mqAnger[i] = math.floor(mqStart + (mqAnger[i] - mqStart) * mqFactor)
 end
-mqAnger[1] = mqStart
+
+-- Calculate time bonus for Doombringer from the number of queens
+local mqNumQueens = spring.GetModOptions().raptor_queen_count or 1
+local mqDoomAngerScale = 1
+if nbQhpMult > 1.3 then mqDoomAngerScale = math.min(10, nbQhpMult / 1.3 * 0.9) end
+local mqDoomAnger = math.ceil(mqDoomAngerScale * (8 * (1.06 ^ math.max(0, mqNumQueens - 8)))) -- base 8 for 8 + exp 1.06x
+local mqAngerBoss = mqTimeMult * 100 + mqDoomAnger  -- baseline 130% anger for 8 queens is 138%
 
 local function newUnit(old, new, data)
 	if unitDefs[old] and not unitDefs[new] then
@@ -116,13 +139,30 @@ newUnit(
 
 unitDefs.raptor_consort.weapondefs.melee = tableCopy(unitDefs[raptor_matriarch_basic].weapondefs.melee)
 
+newUnit(
+	'raptor_consort',
+	'raptor_doombringer',
+	{
+		name = 'Doombringer',
+		icontype = 'armafust3',
+		health = raptor_matriarch_basic_health * 12,
+		speed = 50,
+		customparams = {
+			i18n_en_humanname = 'Doombringer',
+			i18n_en_tooltip = 'Your time is up. The Queens called for backup.'
+		}		
+	}
+)
+
+unitDefs.raptor_doombringer.weapondefs.melee = tableCopy(unitDefs[raptor_matriarch_basic].weapondefs.melee)
+
 local function raptorSquad(p, q, e, r, s, t)
 	return {
 		raptorcustomsquad = true,
 		raptorsquadunitsamount = s or 1,
 		raptorsquadminanger = p,
 		raptorsquadmaxanger = q,
-		raptorsquadweight = t or 1,
+		raptorsquadweight = t or 5,
 		raptorsquadrarity = r or 'basic',
 		raptorsquadbehavior = e,
 		raptorsquadbehaviordistance = 500,
@@ -133,7 +173,7 @@ end
 local miniQueenCommon = {
 	selfdestructas = customfusionexplo,
 	explodeas = customfusionexplo,
-	maxthisunit = scaledMax(4),
+	maxthisunit = scaledMax(3),
 	weapondefs = {
 		yellow_missile = { damage = { default = 1, vtol = 1000 } }
 	}
@@ -155,6 +195,7 @@ for f, u in pairs {
 		}
 	}),
 	raptor_miniq_c = tableMerge(miniQueenCommon, {
+		maxthisunit = scaledMax(4),
 		customparams = raptorSquad(mqAnger[5], mqAnger[6], 'berserk'),
 		weapondefs = {
 			goo = { damage = { default = 3000 } },
@@ -167,28 +208,68 @@ for f, u in pairs {
 		customparams = raptorSquad(mqAnger[2], 1000, 'berserk'),
 		weapondefs = {
 			eyelaser = {
-				range = 800,
+				name = 'Laser Eyes',
+				areaofeffect = 16,
 				reloadtime = 2,
-				damage = {
-					commanders = 6000,
-					default = 6000
-				}
-			},
+				rgbcolor = '1 0 0.3',
+				range = 800,
+				damage = { default = 6000 }
+			},		
 			melee = {
-				name = 'Royal Beak Attack',
-				range = 300,
+				name = 'Aristocratic Beak',
 				soundstart = 'pensquawk1',
-				damage = { default = 1000 }
+				range = 300,
+				reloadtime = 2,
+				damage = { default = 2000 }
 			}
 		},
 		weapons = {
+			[1] = {
+				def = "eyelaser",
+				badtargetcategory = "VTOL",
+			},
 			[2] = {
 				def = 'melee',
 				maindir = '0 0 1',
 				maxangledif = 155
 			}
-		}
+		},
 	},
+	
+	raptor_doombringer = {
+		explodeas = "ScavComBossExplo",
+		maxthisunit = scaledMax(18),
+		customparams = raptorSquad(mqAngerBoss, 1000, 'raider', nil, 3, 99),
+		weapondefs = {
+			eyelaser = {
+				name = 'Eyes of Doom',
+				areaofeffect = 32,
+				reloadtime = 2,
+				rgbcolor = '0.3 1 0',
+				range = 800,			
+				damage = { default = 48000 }
+			},
+			melee = {
+				name = 'Apocalyptic Beak',
+				soundstart = 'penbray2',
+				range = 300,
+				reloadtime = 2,
+				damage = { default = 48000 }
+			}
+		},
+		weapons = {
+			[1] = {
+				def = "eyelaser",
+				badtargetcategory = "",
+				},
+			[2] = {
+				def = 'melee',
+				maindir = '0 0 1',
+				maxangledif = 155
+			}
+		}		
+	},
+	
 	raptor_mama_ba = {
 		maxthisunit = scaledMax(4),
 		customparams = raptorSquad(55, mqAnger[3]-1, 'berserk'),
@@ -222,10 +303,40 @@ for f, u in pairs {
 		customparams = raptorSquad(33, 50, 'raider')
 	},
 	raptor_land_assault_basic_t4_v1 = {
-		maxthisunit = scaledMax(16),
-		customparams = raptorSquad(51, 69, 'raider', 'basic', 2)
+		maxthisunit = scaledMax(12),
+		customparams = raptorSquad(51, 64, 'raider', 'basic', 2)
 	}
 } do
 	unitDefs[f] = unitDefs[f] or {}
 	table.mergeInPlace(unitDefs[f], u, true)
+end
+
+local newCosts = { -- egg drop is 1/3 of this
+	raptor_mama_ba = 36000,
+	raptor_mama_fi = 36000,
+	raptor_mama_el = 36000,
+	raptor_mama_ac = 36000,
+	raptor_consort = 45000,
+	raptor_doombringer = 90000,
+}
+
+local H = UnitDef_Post
+
+function UnitDef_Post(p, I)
+  if H then
+    H(p, I)
+  end
+
+  local nbHpScale = 1
+
+  if nbHpMult > 1.3 then
+    nbHpScale = nbHpMult / 1.3
+  end
+
+  for unitName, baseCost in pairs(newCosts) do
+    if UnitDefs[unitName] then
+      local finalCost = math.floor(baseCost * nbHpScale)
+      UnitDefs[unitName].metalcost = finalCost
+    end
+  end
 end
