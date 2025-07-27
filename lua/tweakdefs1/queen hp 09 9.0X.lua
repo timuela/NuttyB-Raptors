2bfe7045b8a25a019c5e570ebce6aa4aad223c2e
Backbash <149115588+Backbash@users.noreply.github.com>
2025-07-27 18:47:00 -0400

    Character reduction QHP
    Character reduction QHP

--- lua/tweakdefs1/queen hp 09 9.0X.lua
+++ lua/tweakdefs1/queen hp 09 9.0X.lua
@@ -2,11 +2,11 @@
 -- docs.google.com/spreadsheets/d/1QSVsuAAMhBrhiZdTihVfSCwPzbbZWDLCtXWP23CU0ko
 for b, c in pairs(UnitDefs) do
 	if b:match('^raptor_queen_.*') then
-		c.repairable = false
-		c.canbehealed = false
+		c.repairable = 0
+		c.canbehealed = 0
 		c.buildtime = 9999999
 		c.autoheal = 2
-		c.canSelfRepair = false
+		c.canSelfRepair = 0
 		c.health = c.health * 9
 	end
 end
